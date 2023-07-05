b799d9b0109f170e1e373a26382b9e28c5f1c325
gx1285 <gx1285.git@outlook.jp>
2023-07-05 14:23:52 +0900

    fix(autonews): the bot's message will also be made public

--- src/messages/aquedAutoNews.ts
+++ src/messages/aquedAutoNews.ts
@@ -1,7 +1,7 @@
 import { ChannelType, Message } from 'discord.js';
 
 export default async function (message: Message) {
-	if (message.author.bot || message.author.system) return;
+	if (message.author.system) return;
 	if (message.channel.type !== ChannelType.GuildAnnouncement) return;
 	if (!(await message.client.botData.aquedAutoNews.get(message.channelId))) return;
 	if (message.crosspostable) {
