cc32ed890f6d5467309c604536b42432a8feafb3
gx1285 <gx1285.git@outlook.jp>
2024-04-02 23:02:52 +0900

    feat(config): エラーメッセージ削除

--- packages/bot/src/lib/bot/config.ts
+++ packages/bot/src/lib/bot/config.ts
@@ -4,12 +4,5 @@ const discordToken = process.env.DISCORD_TOKEN;
 const discordBotId = process.env.DISCORD_BOT_ID;
 const dataBaseURL = process.env.DATABASE_URL;
 const geminiApiKey = process.env.GEMINI_API_KEY;
-if (!geminiApiKey || typeof geminiApiKey !== 'string')
-	throw new Error('DataBase URLにString以外のものは入れることができません');
-if (!dataBaseURL || typeof dataBaseURL !== 'string')
-	throw new Error('DataBase URLにString以外のものは入れることができません');
-if (!discordBotId || typeof discordBotId !== 'string')
-	throw new Error('Discord Bot IDにString以外のものは入れることができません');
-if (!discordToken || typeof discordToken !== 'string')
-	throw new Error('Discord TokenにString以外のものは入れることができません');
+
 export const Config = { discordToken, dataBaseURL, discordBotId, geminiApiKey };
