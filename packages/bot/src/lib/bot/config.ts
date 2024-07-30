694621708570bd4c464f0a798b1b37370eb9e5cd
gx1285 <contact@gx1285.com>
2024-07-30 13:27:29 +0900

    feat(db): env定義

--- packages/bot/src/lib/bot/config.ts
+++ packages/bot/src/lib/bot/config.ts
@@ -2,7 +2,7 @@ import { config } from 'dotenv';
 config();
 const discordToken = process.env.DISCORD_TOKEN;
 const discordBotId = process.env.DISCORD_BOT_ID;
-const dataBaseURL = process.env.DATABASE_URL;
 const geminiApiKey = process.env.GEMINI_API_KEY;
+const dbHost = process.env.DB_HOST ?? 'db';
 
-export const Config = { discordToken, dataBaseURL, discordBotId, geminiApiKey };
+export const Config = { discordToken, discordBotId, geminiApiKey, dbHost };
