e2fa7b00d6bb209ebb22b3ad044ae84f1b01ef5a
gx1285 <gx1285.git@outlook.jp>
2024-08-23 10:15:51 +0900

    fix(lint): error

--- src/events/ClientReady.ts
+++ src/events/ClientReady.ts
@@ -29,7 +29,7 @@ export default {
 		const logChannel = await client.channels.fetch(client.botData.botLogChannelId);
 		if (!logChannel.isThread()) return;
 		const users: string[] | undefined = await client.botData.commandExecutors.users.get('users');
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		 
 		const commandExecNumber: Array<number | any> = await client.botData.commandExecutors.number.values();
 
 		logChannel.send({
