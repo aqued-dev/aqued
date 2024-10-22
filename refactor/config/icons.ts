e511aea1b1a3cab3e5f4fb4ec9acae3a95f38b83
gx1285 <gx1285.git@outlook.jp>
2024-10-22 18:56:17 +0900

    feat(ui): add icon support

---
+++ refactor/config/icons.ts
@@ -0,0 +1,7 @@
+const baseUrl = 'https://cdn.discordapp.com/emojis/';
+export const icons = {
+	check: baseUrl + '1294246167416275037.png',
+	no: baseUrl + '1298214713221447721',
+	warn: baseUrl + '1298214735078228040',
+	info: baseUrl + '1298214751645601792',
+};
