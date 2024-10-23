075da1b6194fb519f567339bdda9ab732f9ea314
gx1285 <gx1285.git@outlook.jp>
2024-10-23 19:51:01 +0900

    feat: 実行時エラーの埋め込み化

--- src/middlewares/chatInputExecuter.ts
+++ src/middlewares/chatInputExecuter.ts
@@ -1,5 +1,6 @@
 import { ChatInputCommandInteraction, GuildMember } from 'discord.js';
 import { config } from '../config/config.js';
+import { failEmbed } from '../embeds/infosEmbed.js';
 import { translateChannelType } from '../utils/translateChannelType.js';
 import { translatePermission } from '../utils/translatePermission.js';
 
@@ -10,31 +11,40 @@ export const chatInputExecuter = async (interaction: ChatInputCommandInteraction
 		const setting = command.settings;
 		if (!setting.enable) {
 			return await interaction.reply({
-				content: 'このコマンドは無効です！',
+				embeds: [failEmbed('このコマンドは無効です', '実行前エラー')],
 				ephemeral: true
 			});
 		}
 		if (setting.guildOnly && !interaction.inGuild()) {
 			return await interaction.reply({
-				content: 'このコマンドはサーバー内でのみ使用できます！',
+				embeds: [failEmbed('このコマンドはサーバー内でのみ使用できます', '実行前エラー')],
 				ephemeral: true
 			});
 		}
-		if (setting.adminOnly && !config.bot.admins.includes(interaction.user.id))
+		if (setting.adminOnly && !config.bot.admins.includes(interaction.user.id)) {
 			return await interaction.reply({
-				content: 'このコマンドはAquedの管理者のみ使用できます！',
+				embeds: [failEmbed('このコマンドはAquedの管理者のみが使用できます', '実行前エラー')],
 				ephemeral: true
 			});
-		if (setting.modOnly && !config.bot.mods.includes(interaction.user.id))
+		}
+		if (setting.modOnly && !config.bot.mods.includes(interaction.user.id)) {
 			return await interaction.reply({
-				content: 'このコマンドはAquedのモデレーターのみ使用できます！',
+				embeds: [failEmbed('このコマンドはAquedのモデレーターのみが使用できます', '実行前エラー')],
 				ephemeral: true
 			});
+		}
 		if (setting.channelTypes && interaction.channel && !setting.channelTypes.includes(interaction.channel.type)) {
 			return await interaction.reply({
-				content:
-					'このコマンドは以下のチャンネルのみで使用いただけます！\n' +
-					translateChannelType(setting.channelTypes.map((value) => BigInt(value))),
+				embeds: [
+					failEmbed(
+						`このコマンドは以下のチャンネルのみで使用できます\n${translateChannelType(
+							setting.channelTypes.map((value) => BigInt(value))
+						)
+							.map((value) => `\`\`${value}\`\``)
+							.join('\n')}`,
+						'実行前エラー'
+					)
+				],
 				ephemeral: true
 			});
 		}
@@ -51,20 +61,54 @@ export const chatInputExecuter = async (interaction: ChatInputCommandInteraction
 				}
 			}
 			if (insufficient.length !== 0) {
-				return await interaction.reply(
-					`権限が足りません。\n${translatePermission(insufficient)
-						.map((value) => `\`\`${value}\`\``)
-						.join('')}`
-				);
+				return await interaction.reply({
+					embeds: [
+						failEmbed(
+							`このコマンドは実行者に以下の権限が必要です\n${translatePermission(insufficient)
+								.map((value) => `\`\`${value}\`\``)
+								.join('\n')}`,
+							'実行前エラー'
+						)
+					],
+					ephemeral: true
+				});
 			}
 		}
-		if (!interaction.client.aqued.cooldown.has(command.command.name))
+		if (interaction.guild && interaction.channel && !interaction.channel.isDMBased() && setting.permissions) {
+			const bot = interaction.guild.members.cache.get(interaction.client.user.id);
+			if (!bot) {
+				return;
+			}
+			const insufficient: bigint[] = [];
+			for (const value of setting.permissions) {
+				if (!bot.permissionsIn(interaction.channel).has(value)) {
+					insufficient.push(value);
+				}
+			}
+			if (insufficient.length !== 0) {
+				return await interaction.reply({
+					embeds: [
+						failEmbed(
+							`このコマンドはBotに以下の権限が必要です\n${translatePermission(insufficient)
+								.map((value) => `\`\`${value}\`\``)
+								.join('\n')}`,
+							'実行前エラー'
+						)
+					],
+					ephemeral: true
+				});
+			}
+		}
+		if (!interaction.client.aqued.cooldown.has(command.command.name)) {
 			interaction.client.aqued.cooldown.set(command.command.name, new Map());
+		}
 
 		const now = Date.now();
 		const timestamps = interaction.client.aqued.cooldown.get(command.command.name);
 		const cooldownAmount = 3000;
-		if (!timestamps) return;
+		if (!timestamps) {
+			return;
+		}
 		const userCooldown = timestamps.get(interaction.user.id);
 		if (userCooldown) {
 			const expirationTime = userCooldown + cooldownAmount;
@@ -72,7 +116,7 @@ export const chatInputExecuter = async (interaction: ChatInputCommandInteraction
 			if (now < expirationTime) {
 				const timeLeft = (expirationTime - now) / 1000;
 				return await interaction.reply({
-					content: `クールダウン中です。あと、\`${timeLeft.toFixed(1)}\`秒お待ちください。`,
+					embeds: [failEmbed(`あと、\`${timeLeft.toFixed(1)}\`秒お待ちください`, 'クールダウン中')],
 					ephemeral: true
 				});
 			}
@@ -84,8 +128,7 @@ export const chatInputExecuter = async (interaction: ChatInputCommandInteraction
 		return await command.run(interaction);
 	} else {
 		return await interaction.reply({
-			content:
-				'コマンドが存在しません...\n恐らくコマンドがまだ読み込まれていないのでしょう。\nこれは非常に稀です！おめでとうございます',
+			embeds: [failEmbed('このコマンドの処理が読み込まれていません', 'コマンドが存在しません')],
 			ephemeral: true
 		});
 	}
