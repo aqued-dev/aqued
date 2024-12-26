f9a1ec54322e08f05655c26bec073d0bf59ad867
gx1285 <gx1285.git@outlook.jp>
2024-12-26 13:59:55 +0900

    chore: style

--- src/utils/generateCustomId.ts
+++ src/utils/generateCustomId.ts
@@ -5,9 +5,9 @@ export function generateCustomId(
 	name: string,
 	...args: string[]
 ) {
-    let base = `${type}_${component}_${baseName}_${name}`;
-    if (args) {
-        base=base+`_${args.join("_")}`
-    }
+	let base = `${type}_${component}_${baseName}_${name}`;
+	if (args) {
+		base = base + `_${args.join('_')}`;
+	}
 	return base;
 }
