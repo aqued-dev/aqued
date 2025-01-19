4c1d925de90bac15290da1afdd1d13fe35e13a2f
gx1285 <gx1285.git@outlook.jp>
2025-01-19 21:31:11 +0900

    feat: urlcheck復活

--- src/command/chatinput/url_check.ts
+++ src/command/chatinput/url_check.ts
@@ -1,6 +1,34 @@
 import { ChatInputCommandInteraction, Colors, EmbedBuilder } from 'discord.js';
 import { ApplicationIntegrationType, InteractionContextType } from '../../utils/extrans.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
+function embed(type: string, message?: string, title?: string, customTitle?: string, footer?: string) {
+	const embed = new EmbedBuilder();
+	embed.setAuthor({ name: type, iconURL: 'https://cdn.discordapp.com/emojis/1298214751645601792.png' });
+	if (message) {
+		embed.setDescription(message);
+	}
+	if (title) {
+		embed.setTitle(title);
+	}
+	let color: number = Colors.Blue;
+	if (type === '失敗' || type === '削除') {
+		color = Colors.Red;
+	} else if (type === '成功') {
+		color = Colors.Green;
+	} else if (type === '注意') {
+		color = Colors.Yellow;
+	} else if (type === '返信') {
+		color = Colors.Orange;
+	}
+	embed.setColor(color);
+	embed.setFooter({ text: footer ?? 'Aqued' });
+	embed.setTimestamp();
+	if (title) {
+		embed.setTitle(title);
+	}
+
+	return embed;
+}
 export default {
 	command: new SlashCommandBuilder()
 		.setName('url_check')
@@ -13,66 +41,75 @@ export default {
 	permissions: false,
 
 	async execute(interaction: ChatInputCommandInteraction) {
-		return await interaction.error('実行できません', '仕様変更により、現在無効化しています。', true);
 		await interaction.deferReply();
-		const url = interaction.options.getString('url');
-		fetch(`https://safeweb.norton.com/report/show?url=${encodeURI(url)}&ulang=jpn`).then(async (value) => {
-			if (!value.ok)
-				return await interaction.editReply({
-					embeds: [
-						new EmbedBuilder()
-							.setTitle('❌ 確認失敗')
-							.setDescription('安全性の確認に失敗しました。')
-							.setColor(Colors.Red),
-					],
-				});
-			const data: string = await value.text();
-			if (data.includes('安全')) {
-				return await interaction.editReply({
-					embeds: [
-						new EmbedBuilder()
-							.setTitle('このサイトは安全です')
-							.setDescription(`ノートン セーフウェブが分析して安全性とセキュリティの問題を調べました。`)
-							.setColor(Colors.Green)
-
-							.setFooter({ text: 'Powered by Norton Safeweb' }),
-					],
-				});
-			} else if (data.includes('注意')) {
-				return await interaction.editReply({
-					embeds: [
-						new EmbedBuilder()
-							.setTitle('このサイトは注意が必要です')
-							.setDescription(
-								`注意の評価を受けた Web サイトは少数の脅威または迷惑を伴いますが、赤色の警告に相当するほど危険とは見なされません。サイトにアクセスする場合には注意が必要です。`,
-							)
-							.setColor(Colors.Yellow)
-							.setFooter({ text: 'Powered by Norton Safeweb' }),
-					],
-				});
-			} else if (data.includes('警告')) {
-				return await interaction.editReply({
-					embeds: [
-						new EmbedBuilder()
-							.setTitle('このサイトは危険です')
-							.setDescription(`これは既知の危険な Web ページです。このページを表示**しない**ことを推奨します。`)
-							.setColor(Colors.Red)
-							.setFooter({ text: 'Powered by Norton Safeweb' }),
-					],
-				});
-			} else if (data.includes('未評価')) {
-				return new EmbedBuilder()
-					.setTitle('このサイトは未評価です')
-					.setDescription('このサイトはまだ評価されていません。')
-					.setColor(Colors.Grey)
-					.setFooter({ text: 'Powered by Norton Safeweb' });
-			} else {
-				return new EmbedBuilder()
-					.setTitle('このサイトは不明です')
-					.setDescription('仕様変更等により確認ができませんでした。')
-					.setColor(Colors.Grey)
-					.setFooter({ text: 'Powered by Norton Safeweb' });
-			}
-		});
+		const url = interaction.options.getString('url') ?? '';
+		fetch(`https://safeweb.norton.com/safeweb/sites/v1/details?url=${encodeURI(url)}&insert=0`).then(
+			async (response) => {
+				if (!response.ok) {
+					return await interaction.editReply({
+						embeds: [embed('失敗', '安全性の確認に失敗しました', undefined, undefined, 'Powered by Norton Safeweb')],
+					});
+				}
+				const data = (await response.json()) as Record<string, string>;
+				if (!data['rating']) {
+					return await interaction.editReply({
+						embeds: [embed('情報', 'このサイトは不明です', undefined, '不明', 'Powered by Norton Safeweb')],
+					});
+				}
+				if (data['rating'] === 'r' || data['rating'] === 'g') {
+					return await interaction.editReply({
+						embeds: [
+							embed(
+								'成功',
+								'ノートン セーフウェブが分析して安全性とセキュリティの問題を調べました',
+								'このサイトは安全です',
+								undefined,
+								'Powered by Norton Safeweb',
+							),
+						],
+					});
+				}
+				if (data['rating'] === 'w') {
+					return await interaction.editReply({
+						embeds: [
+							embed(
+								'注意',
+								'注意の評価を受けた Web サイトは少数の脅威または迷惑を伴いますが、\n赤色の警告に相当するほど危険とは見なされません\nサイトにアクセスする場合には注意が必要です',
+								'このサイトは注意が必要です',
+								undefined,
+								'Powered by Norton Safeweb',
+							),
+						],
+					});
+				}
+				if (data['rating'] === 'b') {
+					return await interaction.editReply({
+						embeds: [
+							embed(
+								'失敗',
+								'これは既知の危険な Web ページです\nこのページを表示**しない**ことを推奨します',
+								'このサイトは危険です',
+								undefined,
+								'Powered by Norton Safeweb',
+							),
+						],
+					});
+				}
+				if (data['rating'] === 'u') {
+					return await interaction.editReply({
+						embeds: [
+							embed(
+								'情報',
+								'このサイトはまだ評価されていません',
+								'このサイトは未評価です',
+								undefined,
+								'Powered by Norton Safeweb',
+							),
+						],
+					});
+				}
+				return;
+			},
+		);
 	},
 };
