376f97a1678089fc0470fe5c07398136558a44c1
gx1285 <contact@gx1285.com>
2025-02-16 11:43:07 +0900

    chore: migration

--- src/database/migration/1739670310514-DataBase.ts
+++ src/database/migration/1739670310514-DataBase.ts
@@ -1,14 +1,15 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class DataBase1739670310514 implements MigrationInterface {
-    name = 'DataBase1739670310514'
+	name = 'DataBase1739670310514';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`CHANNEL_SETTING\` ADD \`URL_CHECK\` tinyint NULL COMMENT 'チャンネル内自動URLチェック'`);
-    }
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(
+			`ALTER TABLE \`CHANNEL_SETTING\` ADD \`URL_CHECK\` tinyint NULL COMMENT 'チャンネル内自動URLチェック'`
+		);
+	}
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`CHANNEL_SETTING\` DROP COLUMN \`URL_CHECK\``);
-    }
-
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(`ALTER TABLE \`CHANNEL_SETTING\` DROP COLUMN \`URL_CHECK\``);
+	}
 }
--- src/database/migration/1739673555021-DataBase.ts
+++ src/database/migration/1739673555021-DataBase.ts
@@ -1,18 +1,25 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class DataBase1739673555021 implements MigrationInterface {
-    name = 'DataBase1739673555021'
+	name = 'DataBase1739673555021';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE \`SUPER_GLOBAL_CHAT_SEND_IDS\` (\`webhook_url\` text NOT NULL COMMENT '送信に使用したWebhook URL', \`channel_id\` bigint NOT NULL COMMENT 'Webhook URLが削除された場合の作成元チャンネル', \`message_id\` bigint NOT NULL COMMENT '送信済みメッセージID', \`parent_data\` bigint NULL COMMENT 'JSONのメッセージID', PRIMARY KEY (\`message_id\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`ALTER TABLE \`SUPER_GLOBAL_CHAT_DATA\` DROP COLUMN \`send_ids\``);
-        await queryRunner.query(`ALTER TABLE \`SUPER_GLOBAL_CHAT_SEND_IDS\` ADD CONSTRAINT \`FK_21480412ebd394f64a90671bde7\` FOREIGN KEY (\`parent_data\`) REFERENCES \`SUPER_GLOBAL_CHAT_DATA\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION`);
-    }
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(
+			`CREATE TABLE \`SUPER_GLOBAL_CHAT_SEND_IDS\` (\`webhook_url\` text NOT NULL COMMENT '送信に使用したWebhook URL', \`channel_id\` bigint NOT NULL COMMENT 'Webhook URLが削除された場合の作成元チャンネル', \`message_id\` bigint NOT NULL COMMENT '送信済みメッセージID', \`parent_data\` bigint NULL COMMENT 'JSONのメッセージID', PRIMARY KEY (\`message_id\`)) ENGINE=InnoDB`
+		);
+		await queryRunner.query(`ALTER TABLE \`SUPER_GLOBAL_CHAT_DATA\` DROP COLUMN \`send_ids\``);
+		await queryRunner.query(
+			`ALTER TABLE \`SUPER_GLOBAL_CHAT_SEND_IDS\` ADD CONSTRAINT \`FK_21480412ebd394f64a90671bde7\` FOREIGN KEY (\`parent_data\`) REFERENCES \`SUPER_GLOBAL_CHAT_DATA\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION`
+		);
+	}
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`SUPER_GLOBAL_CHAT_SEND_IDS\` DROP FOREIGN KEY \`FK_21480412ebd394f64a90671bde7\``);
-        await queryRunner.query(`ALTER TABLE \`SUPER_GLOBAL_CHAT_DATA\` ADD \`send_ids\` json NOT NULL COMMENT '送信したメッセージIDら'`);
-        await queryRunner.query(`DROP TABLE \`SUPER_GLOBAL_CHAT_SEND_IDS\``);
-    }
-
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(
+			`ALTER TABLE \`SUPER_GLOBAL_CHAT_SEND_IDS\` DROP FOREIGN KEY \`FK_21480412ebd394f64a90671bde7\``
+		);
+		await queryRunner.query(
+			`ALTER TABLE \`SUPER_GLOBAL_CHAT_DATA\` ADD \`send_ids\` json NOT NULL COMMENT '送信したメッセージIDら'`
+		);
+		await queryRunner.query(`DROP TABLE \`SUPER_GLOBAL_CHAT_SEND_IDS\``);
+	}
 }
