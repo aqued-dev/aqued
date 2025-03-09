f865376efc0e912ce3ef0955e4d7314dc299a60c
curtainch <57213007+curtainch@users.noreply.github.com>
2025-03-09 15:43:24 +0900

    build時に発生したエラーを修正

--- src/command/chatinput/finger-war.ts
+++ src/command/chatinput/finger-war.ts
@@ -18,8 +18,8 @@ export default {
   command: new SlashCommandBuilder()
     .setName('finger-war')
     .setDescription('指遊びの戦争を開始！')
-    .setIntegrationTypes(['guild', 'bot_dm']) // ギルド（サーバー）とボットとのDMで利用可能に設定
-    .setContexts(['guild', 'dm']), // サーバー内とDM内で使用可能に設定
+    .setIntegrationTypes(ApplicationIntegrationType.Guild, ApplicationIntegrationType.BotDM) // 修正
+    .setContexts(InteractionContextType.Guild, InteractionContextType.PrivateChannel),
 
 	async execute(interaction: ChatInputCommandInteraction) {
 		const userHands: HandState = { left: 1, right: 1 };
