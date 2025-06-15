fd3cc0224589637e4bc33c12426ae939781cc906
gx1285 <gx1285.git@outlook.jp>
2025-06-15 09:37:59 +0900

    chore: 同期

--- src/interactions/freeChannelCreate.ts
+++ src/interactions/freeChannelCreate.ts
@@ -7,7 +7,7 @@ import {
 	Collection,
 	ModalBuilder,
 	TextInputBuilder,
-	TextInputStyle
+	TextInputStyle,
 } from 'discord.js';
 
 export default async function (interaction: BaseInteraction) {
@@ -66,18 +66,19 @@ export default async function (interaction: BaseInteraction) {
 
 			category.children
 				.create({
-					name: `${interaction.user.discriminator === '0'
-						? interaction.user.globalName || interaction.user.username
-						: interaction.user.username
-						}のチャンネル`,
+					name: `${
+						interaction.user.discriminator === '0'
+							? interaction.user.globalName || interaction.user.username
+							: interaction.user.username
+					}のチャンネル`,
 					topic: `<@!${interaction.user.id}>のチャンネルです。`,
 					type: ChannelType.GuildText,
 				})
 				.then(async (channel) => {
 					const users: string[] = (await interaction.client.botData.aquedFreeChannelUser.get(categoryId)) || [];
 					users.push(interaction.user.id);
-					await channel.lockPermissions()
-					await channel.permissionOverwrites.create(interaction.user.id, { ManageChannels: true })
+					await channel.lockPermissions();
+					await channel.permissionOverwrites.create(interaction.user.id, { ManageChannels: true });
 					await interaction.client.botData.aquedFreeChannelUser.set(categoryId, users);
 					await interaction.ok('チャンネルを作成しました。', `<#${channel.id}>`, true);
 					await channel.send({
