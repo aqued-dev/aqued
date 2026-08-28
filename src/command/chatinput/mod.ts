b432ba041a71a7350f24f80794914a8bf0ef08fb
gx1285 <contact@gx1285.com>
2026-08-28 11:22:19 +0000

    chore: グローバルチャットメッセージ削除コマンドを削除

--- src/command/chatinput/mod.ts
+++ src/command/chatinput/mod.ts
@@ -18,12 +18,6 @@ export default {
 		.setDescription('botモデレーター専用コマンド')
 		.addSubcommand((input) =>
 			input
-				.setName('globalmessagedelete')
-				.setDescription('(botモデレーター専用コマンド)グローバルチャットのメッセージを削除します。')
-				.addStringOption((input) => input.setName('mid').setDescription('メッセージのid').setRequired(true)),
-		)
-		.addSubcommand((input) =>
-			input
 				.setName('iconsync')
 				.setDescription('(botモデレーター専用コマンド)グローバルチャットのwebhookのアイコンを同期します'),
 		)
@@ -40,28 +34,6 @@ export default {
 
 	async execute(interaction: ChatInputCommandInteraction) {
 		switch (interaction.options.getSubcommand()) {
-			case 'globalmessagedelete': {
-				await interaction.deferReply({ ephemeral: true });
-
-				const messages: undefined | { channelId: string; messageId: string }[] =
-					await interaction.client.botData.globalChat.messages.get(interaction.options.getString('mid'));
-				if (!messages) await interaction.error('削除できません', 'メッセージが見つかりません。', true);
-				for (const value of messages) {
-					const channel = interaction.client.channels.cache.get(value.channelId);
-					if (!channel) continue;
-					if (channel.type !== ChannelType.GuildText) continue;
-					const webhooks = await channel.fetchWebhooks();
-					const webhook: Webhook =
-						!webhooks.some((value) => value.name === 'Aqued') ||
-						webhooks.find((value) => value.name === 'Aqued').owner.id !== interaction.client.user.id
-							? await channel.createWebhook({ name: 'Aqued' })
-							: webhooks.find((value) => value.name === 'Aqued');
-
-					webhook.deleteMessage(value.messageId);
-				}
-				await interaction.ok('削除しました。', 'メッセージを削除しました。', true);
-				break;
-			}
 			case 'globalchataquedsystem': {
 				const modal = new ModalBuilder()
 					.setTitle('Aqued System Message')
